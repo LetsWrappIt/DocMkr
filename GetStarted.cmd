01d35e032e5d0a13d7b53b564aa0904364c867bc
PaulHuizer <paulhuizer@gmail.com>
2022-10-23 16:13:28 +0200

    get started

---
+++ GetStarted.cmd
@@ -0,0 +1,25 @@
+# Get started with DocMkr
+
+DocMkr is a command-line utility that you can use to generate proper PDF documents from HTML and Markdwon. This page helps you download DocMkr and get ready with DocMkr.
+
+<a id="download-docmkr"></a>
+## Download DocMkr
+
+First, download the DocMkr executable file to any directory on your computer. DocMkr is just an executable file, so there's nothing to install.
+
+- [Windows 64-bit](https://docmkrapi.azurewebsites.net/docmkr-windows-64bit--latest.zip) (zip)
+- [Windows 64-bit](https://docmkrapi.azurewebsites.net/docmkr-windows-32bit--latest.zip) (zip)
+- [Linux x86-64] -- coming
+- [Linux ARM64 Preview] -- coming
+- [macOS] -- coming
+
+
+For detailed information on DocMkr releases, see the [DocMkr changelog page](changelog.md).
+
+## Run DocMkr
+
+For convenience, consider adding the directory location of the DocMkr executable to your system path for ease of use. That way you can type `docmkr` from any directory on your system.
+
+If you choose not to add the DocMkr directory to your path, you'll have to change directories to the location of your DocMkr executable and type `docmkr` or `.\docmkr` in Windows PowerShell command prompts.
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DocMkr
-DocMkr is a classic scratch-your-own-itch solution. It's a tool that generates PDF documents using HTML and MarkDown.
+DocMkr is a scratch-your-own-itch tool that generates proper PDF documents using HTML and MarkDown.
+
+Some links:
+- [Get started](getstarted.md)
